fce321467d8a320eb088d7e9c4923d100ca42aba
nailseong <nnis9857@gmail.com>
2020-11-19 13:29:08 +0900

    Get Orders

--- src/order/order.resolver.ts
+++ src/order/order.resolver.ts
@@ -1,8 +1,9 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { AuthUser } from 'src/auth/auth-user.decorator';
 import { Role } from 'src/auth/role.decorator';
 import { User } from 'src/users/entities/user.entity';
 import { CreateOrderInput, CreateOrderOutput } from './dtos/create-order.dto';
+import { GetOrdersInput, GetOrdersOutput } from './dtos/get-orders.dto';
 import { Order } from './entities/order.entity';
 import { OrderService } from './order.service';
 
@@ -17,5 +18,14 @@ export class OrderResolver {
     @AuthUser() customer: User,
   ): Promise<CreateOrderOutput> {
     return this.orderService.createOrder(createOrderInput, customer);
+  }
+
+  @Query(returs => GetOrdersOutput)
+  @Role(['Any'])
+  async getOrders(
+    @Args('input') getOrdersInput: GetOrdersInput,
+    @AuthUser() user: User,
+  ): Promise<GetOrdersOutput> {
+    return this.orderService.getOrders(getOrdersInput, user);
   }
 }
--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -2,9 +2,10 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Dish } from 'src/restaurant/entities/dish.entity';
 import { Restaurant } from 'src/restaurant/entities/restaurant.entity';
-import { User } from 'src/users/entities/user.entity';
+import { User, UserRole } from 'src/users/entities/user.entity';
 import { Repository } from 'typeorm';
 import { CreateOrderInput, CreateOrderOutput } from './dtos/create-order.dto';
+import { GetOrdersInput, GetOrdersOutput } from './dtos/get-orders.dto';
 import { OrderItem } from './entities/order-item.entity';
 import { Order } from './entities/order.entity';
 
@@ -84,6 +85,50 @@ export class OrderService {
         }),
       );
       return { ok: true };
+    } catch (error) {
+      return {
+        ok: false,
+        error: 'Unexpected error',
+      };
+    }
+  }
+
+  async getOrders(
+    { status }: GetOrdersInput,
+    user: User,
+  ): Promise<GetOrdersOutput> {
+    try {
+      let orders: Order[];
+      if (user.role === UserRole.Client) {
+        orders = await this.orderRepo.find({
+          where: {
+            customer: user,
+            ...(status && { status }),
+          },
+        });
+      } else if (user.role === UserRole.Delivery) {
+        orders = await this.orderRepo.find({
+          where: {
+            driver: user,
+            ...(status && { status }),
+          },
+        });
+      } else if (user.role === UserRole.Owner) {
+        const restaurants = await this.restaurantRepo.find({
+          where: {
+            owner: user,
+          },
+          relations: ['orders'],
+        });
+        orders = restaurants.map(restaurant => restaurant.orders).flat(1);
+        if (status) {
+          orders = orders.filter(order => order.status === status);
+        }
+      }
+      return {
+        ok: true,
+        orders,
+      };
     } catch (error) {
       return {
         ok: false,
